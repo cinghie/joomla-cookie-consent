f3ea99cb38d46731d704460e31ee1b3efc7f2e78
Giandomenico Olini <giando.olini@gogodigital.it>
2015-11-26 15:53:22 +0100

    Update to first final release

--- gogocookieconsent.php
+++ gogocookieconsent.php
@@ -1,8 +1,14 @@
 <?php
 
 /**
- * @copyright	Copyright (C) Gogodigital Srls. All rights reserved.
- * @license		GNU General Public License version 2 or later
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @email info@gogodigital.it
+ * @github https://github.com/cinghie/joomla-cookie-consent
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 2
+ * @package Gogodigital Cookie Consent
+ * @version 3.0.0
  */
 
 // no direct access
@@ -20,16 +26,24 @@ class plgSystemGogocookieconsent extends JPlugin
 		$app = JFactory::getApplication();		
 		$doc = JFactory::getDocument();
 		
-		if ($app->isAdmin()) 
-		{
+		if ($app->isAdmin()) {
 			return;
 		}
 		
 		$cookieScript = 'window.cookieconsent_options = {
 			"message":"'.$this->params->get("cookieMessage","This website uses cookies to ensure you get the best experience on our website").'",
 			"dismiss":"'.$this->params->get("cookieDismiss","Got It!").'",
-			"learnMore":"'.$this->params->get("cookieMore","More Info").'",
-			"link":'.$this->params->get("cookieLink","null").',
+			"learnMore":"'.$this->params->get("cookieMore","More Info").'",';
+
+		if(!$this->params->get("cookieLink")) {
+			$cookieScript .= '
+				"link": null,';
+		} else {
+			$cookieScript .= '
+				"link":"'.$this->params->get("cookieLink").'",';
+		}
+
+		$cookieScript .= '
 			"theme":"'.$this->params->get("cookieTheme","dark-bottom").'"
 		};';
 		
